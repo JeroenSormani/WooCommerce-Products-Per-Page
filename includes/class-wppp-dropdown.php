380152d17f2a87c30a32b5ddaa332f65fcdb1673
JeroenSormani <jeroen.sormani@gmail.com>
2015-06-09 14:54:19 +0200

    Fix #11 - Refactor plugin structure + move settings page to WC settings

--- includes/class-wppp-dropdown.php
+++ includes/class-wppp-dropdown.php
@@ -14,29 +14,13 @@ class WPPP_Dropdown {
 
 
 	/**
-	 * Product per page option array.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 *
-	 * @var array $product_per_page_options Array of options.
-	 */
-	public $products_per_page_options;
-
-
-	/**
 	 * Construct.
 	 *
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-
-		// Get all the settings
-		$this->settings = WooCommerce_Products_Per_page()->settings;
-
-		// Create the dropdown
-		$this->wppp_dropdown();
-
+		_deprecated_function( array( $this, __FUNCTION__ ), '1.2.0', 'Woocommerce_Products_Per_Page()->front_end->products_per_page_dropdown()' );
+		return $this->front_end->products_per_page_dropdown();
 	}
 
 
@@ -49,7 +33,8 @@ class WPPP_Dropdown {
 	 * @deprecated 1.1.0 Use wppp_dropdown() instead (rename).
 	 */
 	public function wppp_create_object() {
-		$this->wppp_dropdown();
+		_deprecated_function( array( $this, __FUNCTION__ ), '1.2.0', 'Woocommerce_Products_Per_Page()->front_end->products_per_page_dropdown()' );
+		return $this->front_end->products_per_page_dropdown();
 	}
 
 
@@ -63,91 +48,9 @@ class WPPP_Dropdown {
 	 * @global object $wp_query.
 	 */
 	public function wppp_dropdown() {
-
-		global $wp_query;
-
-		$action = '';
-		$cat 	= '';
-		$cat 	= $wp_query->get_queried_object();
-
-		// Set the products per page options (e.g. 4, 8, 12)
-		$products_per_page_options = $this->wppp_prep_ppp( apply_filters( 'wppp_products_per_page', $this->settings['productsPerPage'] ) );
-
-		// Set action url if option behaviour is true
-		// Paste QUERY string after for filter and orderby support
-		$query_string = ! empty( $_SERVER['QUERY_STRING'] ) ? '?' . add_query_arg( array( 'wppp_ppp' => false ), $_SERVER['QUERY_STRING'] ) : null;
-
-		if ( isset( $cat->term_id ) && isset( $cat->taxonomy ) && isset( $this->settings['behaviour'] ) && true == $this->settings['behaviour'] ) :
-			$action = get_term_link( $cat->term_id, $cat->taxonomy ) . $query_string;
-		elseif ( isset( $this->settings['behaviour'] ) &&  true == $this->settings['behaviour'] ) :
-			$action = get_permalink( woocommerce_get_page_id( 'shop' ) ) . $query_string;
-		endif;
-
-		$method = 'post'; // default
-		if ( isset( $this->settings['method'] ) && $this->settings['method'] == 'get' ) :
-			$method = 'get';
-		endif;
-
-		// Only show on product categories
-		if ( woocommerce_products_will_display() ) :
-
-			 do_action( 'wppp_before_dropdown_form' );
-
-			?><form method="<?php echo esc_attr( $method ); ?>" action="<?php echo esc_url( $action ); ?>" style='float: right; margin-left: 5px;' class="form-wppp-select products-per-page"><?php
-
-				 do_action( 'wppp_before_dropdown' );
-
-				?><select name="wppp_ppp" onchange="this.form.submit()" class="select wppp-select"><?php
-
-					foreach( $products_per_page_options as $key => $value ) :
-
-						// Get the right match for the selected option
-						$ppp_session = WC()->session->get( 'products_per_page' );
-						if ( isset( $_POST['wppp_ppp'] ) ) :
-							$selected_match = $_POST['wppp_ppp'];
-						elseif ( isset( $_GET['wppp_ppp'] ) ):
-							$selected_match = $_GET['wppp_ppp'];
-						elseif ( ! empty( $ppp_session ) ) :
-							$selected_match = $ppp_session;
-						else :
-							$selected_match = $this->settings['default_ppp'];
-						endif;
-
-						?><option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, $selected_match ); ?>><?php
-
-							$ppp_text = apply_filters( 'wppp_ppp_text', __( '%s products per page', 'woocommerce-products-per-page' ), $value );
-							printf( $ppp_text, $value == -1 ? __( 'All', 'woocommerce-products-per-page' ) : $value ); // Set to 'All' when value is -1
-
-						?></option><?php
-
-					endforeach;
-
-				?></select><?php
-
-				do_action( 'wppp_after_dropdown' );
-
-			?></form><?php
-
-			do_action( 'wppp_after_dropdown_form' );
-
-		endif;
-
+		_deprecated_function( array( $this, __FUNCTION__ ), '1.2.0', 'Woocommerce_Products_Per_Page()->front_end->products_per_page_dropdown()' );
+		return $this->front_end->products_per_page_dropdown();
 	}
 
-
-	/**
-	 * Prepare dropdown options.
-	 *
-	 * Prepare the options for the products per page dropdown.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return array of options.
-	 */
-	public function wppp_prep_ppp( $products_per_page ) {
-
-		return explode( ' ', $products_per_page );
-
-	}
 
 }
---
+++ includes/class-wppp-front-end.php
@@ -0,0 +1,226 @@
+<?PHP
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Class WPPP_Front_End.
+ *
+ * Handles all front end related business.
+ *
+ * @class		WPPP_Front_End
+ * @version		1.2.0
+ * @author		Jeroen Sormani
+ */
+class WPPP_Front_End {
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.2.0
+	 */
+	public function __construct() {
+
+		// Set dropdowns
+		$location = get_option( 'wppp_dropdown_location', 'none' );
+		if ( $location == 'top' ) :
+			add_action( 'woocommerce_before_shop_loop', array( $this, 'products_per_page_dropdown' ), 25 );
+		elseif ( $location == 'bottom' ) :
+			add_action( 'woocommerce_after_shop_loop', array( $this, 'products_per_page_dropdown' ), 25 );
+		elseif ( $location == 'topbottom' ):
+			add_action( 'woocommerce_before_shop_loop', array( $this, 'products_per_page_dropdown' ), 25 );
+			add_action( 'woocommerce_after_shop_loop', array( $this, 'products_per_page_dropdown' ), 25 );
+		endif;
+
+		// Add filter for product columns
+		add_filter( 'loop_shop_columns', array( $this, 'loop_shop_columns' ) );
+
+		// Custom number of products per page
+		add_filter( 'loop_shop_per_page', array( $this, 'loop_shop_per_page' ) );
+
+		// Get the right amount of products from the DB
+		add_action( 'woocommerce_product_query', array( $this, 'woocommerce_product_query' ), 2, 50 );
+
+		// Set cookie so PPP will be saved
+		add_action( 'init', array( $this, 'set_customer_session' ), 10 );
+
+		// Check if ppp form is fired
+		add_action( 'init', array( $this, 'products_per_page_action' ) );
+
+	}
+
+
+	/**
+	 * Display drop down.
+	 *
+	 * Display the drop down front end to the user to choose
+	 * the number of products per page.
+	 *
+	 * @since 1.2.0
+	 */
+	public function products_per_page_dropdown() {
+
+		global $wp_query;
+
+		$action = '';
+		$cat 	= '';
+		$cat 	= $wp_query->get_queried_object();
+		$method = in_array( get_option( 'wppp_method', 'post' ), array( 'post', 'get' ) ) ? get_option( 'wppp_method', 'post' ) : 'post';
+
+		// Set the products per page options (e.g. 4, 8, 12)
+		$products_per_page_options = explode( ' ', apply_filters( 'wppp_products_per_page', get_option( 'wppp_dropdown_options' ) ) );
+
+		// Set action url if option behaviour is true
+		// Paste QUERY string after for filter and orderby support
+		$query_string = ! empty( $_SERVER['QUERY_STRING'] ) ? '?' . add_query_arg( array( 'wppp_ppp' => false ), $_SERVER['QUERY_STRING'] ) : null;
+
+		if ( isset( $cat->term_id ) && isset( $cat->taxonomy ) && 'yes' == get_option( 'wppp_return_to_first', 'no' ) ) :
+			$action = get_term_link( $cat->term_id, $cat->taxonomy ) . $query_string;
+		elseif ( 'yes' == get_option( 'wppp_return_to_first', 'no' ) ) :
+			$action = get_permalink( woocommerce_get_page_id( 'shop' ) ) . $query_string;
+		endif;
+
+		// Only show on product categories
+		if ( ! woocommerce_products_will_display() ) :
+			return;
+		endif;
+
+		do_action( 'wppp_before_dropdown_form' );
+
+		?><form method="<?php echo $method; ?>" action="<?php echo esc_url( $action ); ?>" style='float: right; margin-left: 5px;' class="form-wppp-select products-per-page"><?php
+
+			 do_action( 'wppp_before_dropdown' );
+
+			?><select name="ppp" onchange="this.form.submit()" class="select wppp-select"><?php
+
+				foreach( $products_per_page_options as $key => $value ) :
+
+					// Get the right match for the selected option
+					$ppp_session = WC()->session->get( 'products_per_page' );
+					if ( isset( $_REQUEST['ppp'] ) ) :
+						$selected_match = $_REQUEST['ppp'];
+					elseif ( isset( $_REQUEST['wppp_ppp'] ) ):
+						$selected_match = $_REQUEST['wppp_ppp'];
+					elseif ( ! empty( $ppp_session ) ) :
+						$selected_match = $ppp_session;
+					else :
+						$selected_match = get_option( 'default_ppp', '12' );
+					endif;
+
+					?><option value="<?php echo esc_attr( $value ); ?>" <?php selected( $value, $selected_match ); ?>><?php
+						$ppp_text = apply_filters( 'wppp_ppp_text', __( '%s products per page', 'woocommerce-products-per-page' ), $value );
+						esc_html( printf( $ppp_text, $value == -1 ? __( 'All', 'woocommerce-products-per-page' ) : $value ) ); // Set to 'All' when value is -1
+					?></option><?php
+
+				endforeach;
+
+			?></select><?php
+
+			do_action( 'wppp_after_dropdown' );
+
+		?></form><?php
+
+		do_action( 'wppp_after_dropdown_form' );
+
+	}
+
+
+	/**
+	 * Shop columns.
+	 *
+	 * Set number of columns (products per row).
+	 *
+	 * @since 1.2.0
+	 *
+	 * @return int Number of columns.
+	 */
+	public function loop_shop_columns( $columns ) {
+
+		if ( ( $shop_columns = get_option( 'wppp_shop_columns', 0 ) ) > 0 ) :
+			$columns = $shop_columns;
+		endif;
+
+		return $columns;
+
+	}
+
+
+	/**
+	 * Per page hook.
+	 *
+	 * Return the number of products per page to the hook
+	 *
+	 * @since 1.2.0
+	 *
+	 * @return int Products per page.
+	 */
+	public function loop_shop_per_page() {
+
+		if ( isset( $_REQUEST['wppp_ppp'] ) ) :
+			return $_REQUEST['wppp_ppp'];
+		elseif ( isset( $_REQUEST['ppp'] ) ) :
+			return $_REQUEST['ppp'];
+		elseif ( WC()->session->__isset( 'products_per_page' ) ) :
+			return WC()->session->__get( 'products_per_page' );
+		else :
+			return get_option( 'wppp_default_ppp', '12' );
+		endif;
+
+	}
+
+
+	/**
+	 * Posts per page.
+	 *
+	 * Set the number of posts per page on a hard way, build in fix for many themes who override the offical loop_shop_per_page filter.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param	object 	$q		Existing query object.
+	 * @param	object	$class	Class object.
+	 * @return 	object 			Modified query object.
+	 */
+	public function woocommerce_product_query( $q, $class ) {
+
+		if ( function_exists( 'woocommerce_products_will_display' ) && woocommerce_products_will_display() && $q->is_main_query() ) :
+			$q->set( 'posts_per_page', $this->loop_shop_per_page() );
+		endif;
+
+	}
+
+
+	/**
+	 * Initilize session.
+	 *
+	 * Set an initial session for WC 2.1.X users. Cookies are set automatically prior 2.1.X.
+	 *
+	 * @since 1.2.0
+	 */
+	public function set_customer_session() {
+
+		if ( WC()->version > '2.1' && ( ! is_admin() || defined( 'DOING_AJAX' ) ) && ! defined( 'DOING_CRON' ) ) :
+			WC()->session->set_customer_session_cookie( true );
+		endif;
+
+	}
+
+
+	/**
+	 * PPP action.
+	 *
+	 * Set the number of products per page when the customer
+	 * changes the amount in the drop down.
+	 *
+	 * @since 1.2.0
+	 */
+	public function products_per_page_action() {
+
+		if ( isset( $_REQUEST['wppp_ppp'] ) ) :
+			WC()->session->set( 'products_per_page', intval( $_REQUEST['wppp_ppp'] ) );
+		elseif ( isset( $_REQUEST['ppp'] ) ) :
+			WC()->session->set( 'products_per_page', intval( $_REQUEST['ppp'] ) );
+		endif;
+
+	}
+
+
+}
