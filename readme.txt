d73c93b9e4aa6752fa885fbe0427d573060d436a
JeroenSormani <jeroen.sormani@gmail.com>
2025-07-15 10:40:05 +0200

    Versioning 1.2.8.1

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: sormano
 Tags: woocommerce, products per page, woocommerce products, woocommerce products per page, products
 Requires at least: 4.0.0
-Tested up to: 6.5
-Stable tag: 1.2.8
+Tested up to: 6.8
+Stable tag: 1.2.8.1
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -68,6 +68,10 @@ Feel free to open a support thread if you need any further help. Please do note 
 
 
 == Changelog ==
+
+= 1.2.8.1 - 15/07/2025 =
+
+* [Fix] - Unintentional error log
 
 = 1.2.8 - 30/05/2024 =
 
--- woocommerce-products-per-page.php
+++ woocommerce-products-per-page.php
@@ -3,13 +3,13 @@
  * Plugin Name: Woocommerce Products Per Page
  * Plugin URI: https://wordpress.org/plugins/woocommerce-products-per-page/
  * Description: Integrate a 'products per page' dropdown on your WooCommerce website! Set-up in <strong>seconds</strong>!
- * Version: 1.2.8
+ * Version: 1.2.8.1
  * Author: Jeroen Sormani
- * Author URI: http://jeroensormani.com
+ * Author URI: https://jeroensormani.com
  * Text Domain: woocommerce-products-per-page
  *
- * WC requires at least: 3.0
- * WC tested up to:      8.9
+ * WC requires at least: 6.0
+ * WC tested up to:      9.9
 
  * Copyright Jeroen Sormani
  *
@@ -51,7 +51,7 @@ class Woocommerce_Products_Per_Page {
 	 * @since 1.2.0
 	 * @var string $version Plugin version number.
 	 */
-	public $version = '1.2.8';
+	public $version = '1.2.8.1';
 
 
 	/**
